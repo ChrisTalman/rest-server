009920aa30c13d20f39265b2ea6d82424a2322a1
Chris Talman <cewe.wow@gmail.com>
2019-03-04 19:46:55 +0000

    Refactored test.

--- Tests/index.js
+++ Tests/index.js
@@ -1,27 +1,28 @@
 'use strict';
 
 // External Modules
-const { default: restServer } = require('../');
+const { default: RestServer } = require('../');
 
 // Resources
 const things = require ('./Resources/Things');
 
+new RestServer
+(
+    {
+        port: 3000,
+        resources:
+        {
+            things
+        },
+        debug:
+        {
+            paths: true
+        }
+    }
+);
 
 setTimeout
 (
-    () => restServer
-    (
-        {
-            port: 3000,
-            resources:
-            {
-                things
-            },
-            debug:
-            {
-                paths: true
-            }
-        }
-    ),
+    () => console.log('Timed out to allow time for dev tools.'),
     4000
 );
