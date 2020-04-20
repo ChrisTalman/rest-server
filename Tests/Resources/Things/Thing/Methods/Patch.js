20634fefacea1409b15d4236916ec54718c00a2e
Chris Talman <cewe.mail@gmail.com>
2020-04-20 12:35:52 +0100

    Introduced console.log().

--- Tests/Resources/Things/Thing/Methods/Patch.js
+++ Tests/Resources/Things/Thing/Methods/Patch.js
@@ -11,5 +11,6 @@ async function handler({request, response})
 	console.log('Raw Body:', request.rawBody);
 	console.log('Text Body:', request.textBody);
 	console.log('JSON Body:', request.body);
+	console.log('Resource Data:', response.locals.resourceData);
 	response.sendStatus(200);
 };
